d3c511ef98d1de279447bb334f40937264f9590c
Keihha <rodrigo.lvzr@gmail.com>
2022-09-27 02:27:19 -0300

    port changed

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { registerUser } = require('./auth/users.controller');
 const { dbConnect} = require('./database');
 const server = express();
-const port = 3000;
+const port = 3001;
 
 // Importaciones generales
 const {setUpMiddleswares} = require('./middlewares/server-middleware');
